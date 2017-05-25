21d0935c8224f5002bc7e8e50571e8cc8f9e83fb
Bryan Sebastian <bryanrsebastian@gmail.com>
2017-05-25 10:25:29 +0800

    update the pattern in preg_match

--- browser_compatibility.php
+++ browser_compatibility.php
@@ -12,16 +12,14 @@ if ( ! function_exists( 'get_user_browser' ) ) {
     {
         $u_agent = $_SERVER['HTTP_USER_AGENT'];
         $u_browser = '';
-        if( preg_match( '/Trident/i', $u_agent ) ) {
+        if( preg_match( '/trident/i', $u_agent ) ) {
             $u_browser = "ie";
-        } elseif( preg_match( '/Firefox/i', $u_agent ) ) {
+        } elseif( preg_match( '/firefox/i', $u_agent ) ) {
             $u_browser = "firefox";
-        } elseif( preg_match( '/Safari/i', $u_agent ) ) {
+        } elseif( preg_match( '/mac/i', $u_agent ) ) {
             $u_browser = "safari";
-        } elseif( preg_match( '/Chrome/i', $u_agent ) ) {
+        } elseif( preg_match( '/chrome/i', $u_agent ) ) {
             $u_browser = "chrome";
-        } elseif( preg_match( '/Flock/i', $u_agent ) ) {
-            $u_browser = "flock";
         } elseif( preg_match( '/Opera/i',$u_agent ) || preg_match( '/OPR/i',$u_agent ) ) {
             $u_browser = "opera";
         }
